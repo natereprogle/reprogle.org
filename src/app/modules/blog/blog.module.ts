b7fb42aed13d146f8cc7c0f6d03a1efd3f3655dd
Nate Reprogle <4064388+TerrrorByte@users.noreply.github.com>
2023-11-12 14:44:17 -0600

    Update to Angular v17

--- src/app/modules/blog/blog.module.ts
+++ src/app/modules/blog/blog.module.ts
@@ -1,26 +1,24 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import {BlogRoutingModule} from './blog-routing.module';
-import {BlogComponent} from './pages/blog/blog.component';
-import {initializeApp, provideFirebaseApp} from "@angular/fire/app";
-import {environment} from "../../../environments/environment";
-import {getFirestore, provideFirestore} from "@angular/fire/firestore";
-import {getRemoteConfig, provideRemoteConfig} from "@angular/fire/remote-config";
-import {MarkdownModule, MarkedOptions} from "ngx-markdown";
-import {HttpClient} from "@angular/common/http";
-import {markedOptionsFactory} from "../main/services/markdown-factory";
-import {BlogCardComponent} from "./components/blog-card/blog-card.component";
-import {BlogPostComponent} from "./pages/blog-post/blog-post.component";
-import {SharedModule} from "../shared/shared.module";
-
+import { BlogRoutingModule } from './blog-routing.module';
+import { BlogComponent } from './pages/blog/blog.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../../../environments/environment';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import {
+  getRemoteConfig,
+  provideRemoteConfig,
+} from '@angular/fire/remote-config';
+import { MARKED_OPTIONS, MarkdownModule } from 'ngx-markdown';
+import { HttpClient } from '@angular/common/http';
+import { markedOptionsFactory } from '../main/services/markdown-factory';
+import { BlogCardComponent } from './components/blog-card/blog-card.component';
+import { BlogPostComponent } from './pages/blog-post/blog-post.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    BlogComponent,
-    BlogCardComponent,
-    BlogPostComponent
-  ],
+  declarations: [BlogComponent, BlogCardComponent, BlogPostComponent],
   imports: [
     CommonModule,
     BlogRoutingModule,
@@ -31,11 +29,10 @@ import {SharedModule} from "../shared/shared.module";
     MarkdownModule.forRoot({
       loader: HttpClient,
       markedOptions: {
-        provide: MarkedOptions,
+        provide: MARKED_OPTIONS,
         useFactory: markedOptionsFactory,
       },
     }),
-  ]
+  ],
 })
-export class BlogModule {
-}
+export class BlogModule {}
